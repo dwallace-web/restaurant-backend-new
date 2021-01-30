9227334e3454eed84575f99f9acb90ef6865f945
David Wallace <dwallacemarketing@gmail.com>
2021-01-30 14:03:53 -0500

    ssl handling

--- db.js
+++ db.js
@@ -6,7 +6,13 @@ const Sequelize = require('sequelize');
 // });
 
 const database = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres'
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // <<<<<<< YOU NEED THIS TO FIX UNHANDLED REJECTION 
+    },
+  },
 });
 
 database
