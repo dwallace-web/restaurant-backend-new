369a7134facba46822593a2c5151c17e58213ad1
David Wallace <dwallacemarketing@gmail.com>
2021-01-30 10:07:44 -0500

    prep server deployment

--- db.js
+++ db.js
@@ -1,8 +1,12 @@
 const Sequelize = require('sequelize');
 
-const database = new Sequelize(process.env.NAME, 'postgres', process.env.PASS, {
-  host: 'localhost',
-  dialect: 'postgres',
+// const database = new Sequelize(process.env.NAME, 'postgres', process.env.PASS, {
+//   host: 'localhost',
+//   dialect: 'postgres',
+// });
+
+const database = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres'
 });
 
 database
